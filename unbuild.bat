07c218594515a22ef645ce2cfaf69cdfe7f3584a
Tom Willemsen <tomwillemsen1995@gmail.com>
2019-04-23 11:01:52 +0100

    Make build work on VS2017

--- unbuild.bat
+++ unbuild.bat
@@ -1,19 +1,19 @@
 @echo off
 
 if /I "%EPICS_HOST_ARCH%" == "windows-x64-debug" (
-    call msbuild.exe /p:Configuration=Debug;Platform=x64 /t:clean AstriumChopper.sln
+    call msbuild.exe /p:Configuration=Debug;Platform=x64 /t:clean %1
 )
 
 if /I "%EPICS_HOST_ARCH%" == "windows-x64" (
-    call msbuild.exe /p:Configuration=Release;Platform=x64 /t:clean AstriumChopper.sln
+    call msbuild.exe /p:Configuration=Release;Platform=x64 /t:clean %1
 )
     
 if /I "%EPICS_HOST_ARCH%" == "win32-x86-debug" (
-    call msbuild.exe /p:Configuration=Debug;Platform=x86 /t:clean AstriumChopper.sln
+    call msbuild.exe /p:Configuration=Debug;Platform=x86 /t:clean %1
 )
 
 if /I "%EPICS_HOST_ARCH%" == "win32-x86" (
-    call msbuild.exe /p:Configuration=Release;Platform=x86 /t:clean AstriumChopper.sln
+    call msbuild.exe /p:Configuration=Release;Platform=x86 /t:clean %1
 )
 
 if %ERRORLEVEL% neq 0 goto PROBLEM
