feaaffd809c41564736519ef7aef36d5220871a8
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2024-09-02 13:54:09 +0100

    Set .NET version based on Visual Studio

--- build.bat
+++ build.bat
@@ -4,54 +4,56 @@ REM Create an EPICS style layout
 mkdir bin\%EPICS_HOST_ARCH%
 mkdir lib\%EPICS_HOST_ARCH%
 
+call %~dp0vcversion.bat
+
 if /I "%EPICS_HOST_ARCH%" == "windows-x64-debug" (
     set BUILD_DIR=x64\Debug
-    call msbuild.exe /p:Configuration=Debug;Platform=x64 %1
+    call msbuild.exe /p:Configuration=Debug;Platform=x64;PlatformToolset=%PLATFORM_TOOLSET%;TargetFrameworkVersion=%TARGET_FRAMEWORK_VERSION% %1
 )
 
 if /I "%EPICS_HOST_ARCH%" == "windows-x64" (
     set BUILD_DIR=x64\Release
-    call msbuild.exe /p:Configuration=Release;Platform=x64 %1
+    call msbuild.exe /p:Configuration=Release;Platform=x64;PlatformToolset=%PLATFORM_TOOLSET%;TargetFrameworkVersion=%TARGET_FRAMEWORK_VERSION% %1
 )
     
 if /I "%EPICS_HOST_ARCH%" == "windows-x64-static-debug" (
     set BUILD_DIR=x64\Debug
-    call msbuild.exe /p:Configuration=Debug;Platform=x64 %1
+    call msbuild.exe /p:Configuration=Debug;Platform=x64;PlatformToolset=%PLATFORM_TOOLSET%;TargetFrameworkVersion=%TARGET_FRAMEWORK_VERSION% %1
 )
 
 if /I "%EPICS_HOST_ARCH%" == "windows-x64-static" (
     set BUILD_DIR=x64\Release
-    call msbuild.exe /p:Configuration=Release;Platform=x64 %1
+    call msbuild.exe /p:Configuration=Release;Platform=x64;PlatformToolset=%PLATFORM_TOOLSET%;TargetFrameworkVersion=%TARGET_FRAMEWORK_VERSION% %1
 )
 
 if /I "%EPICS_HOST_ARCH%" == "windows-x64-static_md-debug" (
     set BUILD_DIR=x64\Debug
-    call msbuild.exe /p:Configuration=Debug;Platform=x64 %1
+    call msbuild.exe /p:Configuration=Debug;Platform=x64;PlatformToolset=%PLATFORM_TOOLSET%;TargetFrameworkVersion=%TARGET_FRAMEWORK_VERSION% %1
 )
 
 if /I "%EPICS_HOST_ARCH%" == "windows-x64-static_md" (
     set BUILD_DIR=x64\Release
-    call msbuild.exe /p:Configuration=Release;Platform=x64 %1
+    call msbuild.exe /p:Configuration=Release;Platform=x64;PlatformToolset=%PLATFORM_TOOLSET%;TargetFrameworkVersion=%TARGET_FRAMEWORK_VERSION% %1
 )
 
 if /I "%EPICS_HOST_ARCH%" == "win32-x86-debug" (
     set BUILD_DIR=Debug
-    call msbuild.exe /p:Configuration=Debug;Platform=x86 %1
+    call msbuild.exe /p:Configuration=Debug;Platform=x86;PlatformToolset=%PLATFORM_TOOLSET%;TargetFrameworkVersion=%TARGET_FRAMEWORK_VERSION% %1
 )
 
 if /I "%EPICS_HOST_ARCH%" == "win32-x86" (
     set BUILD_DIR=Release
-    call msbuild.exe /p:Configuration=Release;Platform=x86 %1
+    call msbuild.exe /p:Configuration=Release;Platform=x86;PlatformToolset=%PLATFORM_TOOLSET%;TargetFrameworkVersion=%TARGET_FRAMEWORK_VERSION% %1
 )
 
 if /I "%EPICS_HOST_ARCH%" == "win32-x86-static-debug" (
     set BUILD_DIR=Debug
-    call msbuild.exe /p:Configuration=Debug;Platform=x86 %1
+    call msbuild.exe /p:Configuration=Debug;Platform=x86;PlatformToolset=%PLATFORM_TOOLSET%;TargetFrameworkVersion=%TARGET_FRAMEWORK_VERSION% %1
 )
 
 if /I "%EPICS_HOST_ARCH%" == "win32-x86-static" (
     set BUILD_DIR=Release
-    call msbuild.exe /p:Configuration=Release;Platform=x86 %1
+    call msbuild.exe /p:Configuration=Release;Platform=x86;PlatformToolset=%PLATFORM_TOOLSET%;TargetFrameworkVersion=%TARGET_FRAMEWORK_VERSION% %1
 )
 
 if %ERRORLEVEL% neq 0 goto PROBLEM
--- unbuild.bat
+++ unbuild.bat
@@ -1,19 +1,21 @@
 @echo off
 
+call %~dp0vcversion.bat
+
 if /I "%EPICS_HOST_ARCH%" == "windows-x64-debug" (
-    call msbuild.exe /p:Configuration=Debug;Platform=x64 /t:clean %1
+    call msbuild.exe /p:Configuration=Debug;Platform=x64;PlatformToolset=%PLATFORM_TOOLSET%;TargetFrameworkVersion=%TARGET_FRAMEWORK_VERSION% /t:clean %1
 )
 
 if /I "%EPICS_HOST_ARCH%" == "windows-x64" (
-    call msbuild.exe /p:Configuration=Release;Platform=x64 /t:clean %1
+    call msbuild.exe /p:Configuration=Release;Platform=x64;PlatformToolset=%PLATFORM_TOOLSET%;TargetFrameworkVersion=%TARGET_FRAMEWORK_VERSION% /t:clean %1
 )
     
 if /I "%EPICS_HOST_ARCH%" == "win32-x86-debug" (
-    call msbuild.exe /p:Configuration=Debug;Platform=x86 /t:clean %1
+    call msbuild.exe /p:Configuration=Debug;Platform=x86;PlatformToolset=%PLATFORM_TOOLSET%;TargetFrameworkVersion=%TARGET_FRAMEWORK_VERSION% /t:clean %1
 )
 
 if /I "%EPICS_HOST_ARCH%" == "win32-x86" (
-    call msbuild.exe /p:Configuration=Release;Platform=x86 /t:clean %1
+    call msbuild.exe /p:Configuration=Release;Platform=x86;PlatformToolset=%PLATFORM_TOOLSET%;TargetFrameworkVersion=%TARGET_FRAMEWORK_VERSION% /t:clean %1
 )
 
 if %ERRORLEVEL% neq 0 goto PROBLEM
